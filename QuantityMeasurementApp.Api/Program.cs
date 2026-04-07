2a435523aff66ef2bb4ccea9e655d3fb10a2f50c
Kur0._. <animeshrajpoot004@gmail.com>
2026-04-07 11:12:52 +0530

    [Animesh Rajpoot] Refactor.Fixed Swagger

--- QuantityMeasurementApp.Api/Program.cs
+++ QuantityMeasurementApp.Api/Program.cs
@@ -172,15 +172,8 @@ if (!app.Environment.IsEnvironment("Testing"))
     await db.Database.MigrateAsync();
 }
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Quantity Measurement API v1");
-        c.RoutePrefix = string.Empty;
-    });
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseCors("AllowFrontend");
